66150bf3bf5865a2e13dcb105107ff8f9dbc25ee
Christian Gmeiner <christian.gmeiner@gmail.com>
2013-05-24 18:12:01 +0000

    v4/imx6: do not try to modify kernel created context
    The kernel driver does setup the context for us and
    returns a kernel space pointer (ctx->ctx).
    It is a really bad idea to try to modify it.

--- native/etnaviv/etna.c
+++ native/etnaviv/etna.c
@@ -317,12 +317,8 @@ int etna_flush(struct etna_ctx *ctx)
 #endif
     /* TODO: analyze command buffer to update context */
     /* set context entryPipe to currentPipe (next commit will start with current pipe) */
-#ifdef GCABI_HAS_CONTEXT
     ctx->ctx.entryPipe = ctx->ctx.currentPipe;
-#else
-    ctx->ctx.entryPipe = gcvPIPE_3D;
-    ctx->ctx.exitPipe = gcvPIPE_3D;
-#endif
+
     /* TODO: update context, NOP out final pipe2D if entryPipe is 2D, else put in the 2D pipe switch */
     /* TODO: if context was used, queue it to be freed later, and initialize new context buffer */
     cur_buf->startOffset = cur_buf->offset + END_COMMIT_CLEARANCE;
