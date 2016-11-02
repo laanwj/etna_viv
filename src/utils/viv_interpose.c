8c5fd8cfa0de86cb7551443620903c179565e31a
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-02 09:48:49 +0000

    Add interposer library for command stream interception
    This is easier to use than having to call viv_hook manually.
    
        LD_PRELOAD="./viv_interpose.so" ...

---
+++ src/utils/viv_interpose.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Wladimir J. van der Laan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sub license,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+#include "viv_hook.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <pthread.h>
+
+static void __attribute__((constructor)) constructor()
+{
+    printf("Constructor\n");
+    the_hook("out.fdr");
+}
+
+static void __attribute__((destructor)) destructor()
+{
+    printf("Destructor\n");
+    close_hook();
+}
+
