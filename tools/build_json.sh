8b5b317653f79688caee28281528bb80d776bc5d
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-02 08:07:45 +0000

    Make it clear json is GCABI dependent

--- tools/build_json.sh
+++ tools/build_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # build structure definition for GCS hal interface from dwarf debug information in native executable.
 # Needs dwarf_to_c (https://www.github.com/laanwj/dwarf_to_c)
-extract_structures_json.py ../native/egl/cube _gcsHAL_INTERFACE > data/gcs_hal_interface.json
+extract_structures_json.py ../native/egl/cube _gcsHAL_INTERFACE > data/gcs_hal_interface_${GCABI}.json
